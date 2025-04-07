ec5056bb217ada698f0289f41ed16b4d679a487c
Madhu-shree1243 <114743017+Madhu-shree1243@users.noreply.github.com>
2025-04-07 17:48:57 +0530

    done2

--- Largesteleina.java
+++ Largesteleina.java
@@ -1,15 +1,32 @@
 import java.io.*;
 import java.util.Arrays;
 public class Largesteleina{
-static private void getElements(int[] arr,int n){
-     Arrays.sort(arr);
-     int large = arr[n-1];
-     System.out.println(" largest is " + large);
+static private void getLargest(int[] arr,int n){
+//      Arrays.sort(arr);
+//      int large = arr[n-1];
+//      System.out.println(" largest is " + large);
 
+// }
+// public static void main(String args[]){ ---------------->>>>> brute force
+//      int[] arr = {1,2,6,4,3,9};
+//      int n = arr.length;
+//      getElements(arr, n);
+// }
+
+//-------------------------Optimal Solution------------------------------
+  int largest = arr[0];
+ for(int i=1;i<n;i++){
+     if(arr[i]>largest){
+          largest = arr[i];
+     }
+ }
+ System.out.println("largest is " + largest);
+ 
 }
-public static void main(String args[]){
+public static void main(String args[]){ 
      int[] arr = {1,2,6,4,3,9};
      int n = arr.length;
-     getElements(arr, n);
+     getLargest(arr, n);
+     
 }
 }
